7ecbf414cf2d27ee43e7567d854bc72e1e24a1c2
Md Nayeem Hossain <nayeemhossain108@gmail.com>
2021-09-02 18:16:29 +0600

    secound time push

--- app.js
+++ app.js
@@ -5,8 +5,10 @@ const searchBook=()=>{
     const searchText=searchInput.value;
     // serch result condition check
     if(searchText===''){
-      bookSection.innerHTML=`<h3 class="bg-danger p-5 text-center">Please type valid book name<h3>
+      createP.innerText=''
+      bookSection.innerHTML=`<h3 class="text-danger  text-center">Invalid Book name.Please type valid book name<h3>
       `
+      
     }
     else{
       
@@ -26,32 +28,33 @@ const searchBook=()=>{
 }
 const displaySearchResult=(books)=>{
   // use filter for good and expected output
-  const filterResult=books.filter(search=>search.author_name !== undefined && search.cover_i !== undefined && search.title !== undefined && search.first_publish_year !== undefined)
+  const filterResult=books.filter(search=>search.author_name !== undefined && search.cover_i !== undefined && search.title !== undefined && search.first_publish_year !== undefined && search.publisher !== undefined)
     // console.log(filterResult)
     const displayResult=document.getElementById('search-result')
     const searchQuantity=document.getElementById('search-quantity')
     // new document create
     const createP=document.createElement('p')
-createP.innerText=`You have found ${filterResult.length} books`
+createP.innerText=`You have ${filterResult.length} results found`
     displayResult.textContent=''
     searchQuantity.appendChild(createP)
   //  use forEach function
 filterResult.forEach(book => {
     const div=document.createElement('div')
     div.classList.add('col');
-    div.innerHTML=`<div class="card p-2" style="width: 22rem; height:500px">
+    div.innerHTML=`<div class="card shadow p-3 mb-5 rounded" style="width: 22rem; height:500px">
     <img style="height:300px" class="card-img-top" alt="..." src="https://covers.openlibrary.org/b/id/${book.cover_i}-M.jpg">
     <div class="card-body">
-      <h6 class="card-title ">Book Title: ${book.title}</h6>
-      <h6 class="card-title text-primary">Author Name: ${book.author_name[0].slice(0,15)}</h6>
-      <h6 class="card-title text-success"> First Publish Year: ${book.first_publish_year}</h6>
-      
+      <h6 class="card-title ">Book Title: <span class="text-primary"> ${book.title}</span> </h6>
+      <h6 class="card-title">Author Name: <span class="text-primary">${book.author_name[0].slice(0,15)}</span> </h6>
+      <h6 class="card-title"> Publisher: <span class="text-primary">${book.publisher[0]}</span</h6>
+
+      <h6 class="card-title ">First Publish Date: <span class="text-primary"> ${book.first_publish_year}</span> </h6>
     </div>
   </div>
     `
     displayResult.appendChild(div)
 
-});
+}); 
 }
 
     
